1cfbf274bcb1535fbb711ca10a554d16d5c4bf2c
aiwenli123 <ali52@hawk.iit.edu>
2025-06-18 09:36:21 -0400

    elapsed_seconds eda and got rid of points with super long elapsed_seconds

--- new code/09_more_eda.R
+++ new code/09_more_eda.R
@@ -64,3 +64,172 @@ ggplot(f_second_welos, aes(x = server_welo, y = speed_ratio)) +
   theme_minimal()
 ggsave("../images/female_welo_vs_second_serve_speed.png", bg = "white", 
        width = 8, height = 6, units = "in")
+
+## server wElo vs. serve location
+m_categorical_welos <- subset_m_second %>%
+  mutate(server_welo = if_else(PointServer == 1, player1_avg_welo, player2_avg_welo))
+
+# Boxplot: Server wElo vs. ServeWidth
+ggplot(m_categorical_welos, aes(x = ServeWidth, y = server_welo)) +
+  geom_boxplot() +
+  labs(title = "Server wElo by Serve Width (Males)",
+       x = "Serve Width",
+       y = "Server wElo") +
+  theme_minimal()
+ggsave("../images/male_welo_vs_servewidth.png", bg = "white", width = 7, height = 5, units = "in")
+
+# Boxplot: Server wElo vs. ServeDepth
+ggplot(m_categorical_welos, aes(x = ServeDepth, y = server_welo)) +
+  geom_boxplot() +
+  labs(title = "Server wElo by Serve Depth (Males)",
+       x = "Serve Depth",
+       y = "Server wElo") +
+  theme_minimal()
+ggsave("../images/male_welo_vs_servedepth.png", bg = "white", width = 7, height = 5, units = "in")
+
+f_categorical_welos <- subset_f_second %>%
+  mutate(server_welo = if_else(PointServer == 1, player1_avg_welo, player2_avg_welo))
+
+# Boxplot: Server wElo vs. ServeWidth
+ggplot(f_categorical_welos, aes(x = ServeWidth, y = server_welo)) +
+  geom_boxplot() +
+  labs(title = "Server wElo by Serve Width (Females)",
+       x = "Serve Width",
+       y = "Server wElo") +
+  theme_minimal()
+ggsave("../images/female_welo_vs_servewidth.png", bg = "white", width = 7, height = 5, units = "in")
+
+# Boxplot: Server wElo vs. ServeDepth
+ggplot(f_categorical_welos, aes(x = ServeDepth, y = server_welo)) +
+  geom_boxplot() +
+  labs(title = "Server wElo by Serve Depth (Females)",
+       x = "Serve Depth",
+       y = "Server wElo") +
+  theme_minimal()
+ggsave("../images/female_welo_vs_servedepth.png", bg = "white", width = 7, height = 5, units = "in")
+
+#-----------------------------------------------------------------------------------------------------
+
+# elapsed_seconds vs. serve speed ratio and/or serve location (does strategy change as match goes on?)
+
+m_second_elapsed <- subset_m_second %>%
+  select(ElapsedSeconds, speed_ratio)
+
+cor(m_second_elapsed$ElapsedSeconds, m_second_elapsed$speed_ratio, use = "complete.obs") # 0.04 ish
+
+ggplot(m_second_elapsed, aes(x = ElapsedSeconds, y = speed_ratio)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Seconds Elapsed (Match) vs. Second Serve Speed Ratio -- Males (corr ~ 0.04)",
+       x = "Seconds Elapsed",
+       y = "Second Serve Speed Ratio (compared to avg first serve speed)") +
+  theme_minimal()
+
+## weird
+## plot distn of ElapsedSeconds for second serves
+ggplot(m_second_elapsed, aes(x = ElapsedSeconds)) +
+  geom_histogram(binwidth = 2, fill = "steelblue", color = "white", alpha = 0.8) +
+  labs(title = "Distribution of Elapsed Time (Second Serves)",
+       x = "Elapsed Seconds",
+       y = "Count") +
+  theme_minimal()
+
+unique_times <- unique(c(m_second_elapsed$ElapsedSeconds))
+summary(unique_times)
+# re-order unique_times in ascending order
+unique_times <- sort(unique_times)
+
+# create a data frame with counts of each unique time
+time_counts <- as.data.frame(table(m_second_elapsed$ElapsedSeconds))
+
+## some matches last super long (over 8 hours for men, and over 6 hours for women)
+
+#-----------------------------------------------------------------------------------------------------
+
+## new elapsedtime eda
+subset_m <- subset_m[ElapsedSeconds < 28800] # 8 hours
+subset_f <- subset_f[ElapsedSeconds < 21600] # 6 hours
+
+subset_m_second <- subset_m[ServeNumber == 2]
+subset_f_second <- subset_f[ServeNumber == 2]
+
+m_second_elapsed <- subset_m_second %>%
+  select(ElapsedSeconds, speed_ratio)
+
+cor(m_second_elapsed$ElapsedSeconds, m_second_elapsed$speed_ratio, use = "complete.obs") # -0.05 ish
+
+ggplot(m_second_elapsed, aes(x = ElapsedSeconds, y = speed_ratio)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Seconds Elapsed (Match) vs. Second Serve Speed Ratio -- Males (corr ~ -0.05)",
+       x = "Seconds Elapsed",
+       y = "Second Serve Speed Ratio (compared to avg first serve speed)") +
+  theme_minimal()
+ggsave("../images/male_time_vs_second_serve_speed.png", bg = "white", 
+       width = 8, height = 6, units = "in")
+
+f_second_welos <- subset_f_second %>% 
+  mutate(server_welo = if_else(PointServer == 1, player1_avg_welo, player2_avg_welo)) %>%
+  select(server_welo, speed_ratio)
+
+cor(f_second_welos$server_welo, f_second_welos$speed_ratio, use = "complete.obs") # -0.14 ish
+
+ggplot(f_second_welos, aes(x = server_welo, y = speed_ratio)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Seconds Elapsed (Match) vs. Second Serve Speed Ratio -- Females (corr ~ -0.14)",
+       x = "Seconds Elapsed",
+       y = "Second Serve Speed Ratio (compared to avg first serve speed)") +
+  theme_minimal()
+ggsave("../images/female_time_vs_second_serve_speed.png", bg = "white", 
+       width = 8, height = 6, units = "in")
+
+#-----------------------------------------------------------------------------------------------------
+
+## seconds elapsed vs. serve location
+m_categorical_welos <- subset_m_second %>%
+  mutate(server_welo = if_else(PointServer == 1, player1_avg_welo, player2_avg_welo))
+
+# Boxplot: ServeWidth
+ggplot(m_categorical_welos, aes(x = ServeWidth, y = ElapsedSeconds)) +
+  geom_boxplot() +
+  labs(title = "Seconds Elapsed by Serve Width (Males)",
+       x = "Serve Width",
+       y = "Seconds Elapsed") +
+  theme_minimal()
+ggsave("../images/male_time_vs_servewidth.png", bg = "white", width = 7, height = 5, units = "in")
+
+# Boxplot: ServeDepth
+ggplot(m_categorical_welos, aes(x = ServeDepth, y = ElapsedSeconds)) +
+  geom_boxplot() +
+  labs(title = "Seconds Elapsed by Serve Depth (Males)",
+       x = "Serve Depth",
+       y = "Seconds Elapsed") +
+  theme_minimal()
+ggsave("../images/male_time_vs_servedepth.png", bg = "white", width = 7, height = 5, units = "in")
+
+f_categorical_welos <- subset_f_second %>%
+  mutate(server_welo = if_else(PointServer == 1, player1_avg_welo, player2_avg_welo))
+
+# Boxplot: Server wElo vs. ServeWidth
+ggplot(f_categorical_welos, aes(x = ServeWidth, y = ElapsedSeconds)) +
+  geom_boxplot() +
+  labs(title = "Server wElo by Serve Width (Females)",
+       x = "Serve Width",
+       y = "Seconds Elapsed") +
+  theme_minimal()
+ggsave("../images/female_time_vs_servewidth.png", bg = "white", width = 7, height = 5, units = "in")
+
+# Boxplot: Server wElo vs. ServeDepth
+ggplot(f_categorical_welos, aes(x = ServeDepth, y = ElapsedSeconds)) +
+  geom_boxplot() +
+  labs(title = "Seconds Elapsed by Serve Depth (Females)",
+       x = "Serve Depth",
+       y = "Seconds Elapsed") +
+  theme_minimal()
+ggsave("../images/female_time_vs_servedepth.png", bg = "white", width = 7, height = 5, units = "in")
+
+write.csv(subset_m, "../data/wimbledon_subset_m_09.csv", row.names = FALSE)
+write.csv(subset_f, "../data/wimbledon_subset_f_09.csv", row.names = FALSE)
+
+#-----------------------------------------------------------------------------------------------------
