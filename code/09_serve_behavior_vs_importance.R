72bec32418ad03cf5c4df1dd655fcc2348eb59e1
aiwenli123 <ali52@hawk.iit.edu>
2025-07-23 11:43:22 -0400

    clean

--- code/09_serve_behavior_vs_importance.R
+++ code/09_serve_behavior_vs_importance.R
@@ -80,3 +80,26 @@ ggsave("../data/results/importance_analysis/sd_speed_vs_importance.png", p2, wid
 ggsave("../data/results/importance_analysis/ace_pct_vs_importance.png", p3, width = 6, height = 4, bg = "white")
 ggsave("../data/results/importance_analysis/entropy_vs_importance.png", p4, width = 6, height = 4, bg = "white")
 
+#REGRESSION
+lm1 <- lm(avg_speed ~ importance, data = importance_summary)
+lm2 <- lm(sd_speed ~ importance, data = importance_summary)
+lm3 <- lm(ace_pct ~ importance, data = importance_summary)
+lm4 <- lm(location_entropy ~ importance, data = importance_summary)
+
+sink("../data/results/importance_analysis/linear_model_summaries.txt")
+cat("Regression Summaries:\n\n")
+summary(lm1)
+summary(lm2)
+summary(lm3)
+summary(lm4)
+sink()
+
+#Importance Frequency
+modal_counts <- df_clean %>%
+    group_by(importance, location_bin) %>%
+    summarise(n = n(), .groups = "drop") %>%
+    group_by(importance) %>%
+    mutate(prop = n / sum(n)) %>%
+    pivot_wider(names_from = location_bin, values_from = prop, values_fill = 0)
+
+write.csv(modal_counts, "../data/results/importance_analysis/modal_location_props.csv", row.names = FALSE)
